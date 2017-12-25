3ffe4d3abe3e4db781df45d3e2a3fbac30f8ab6c
eduardo305 <eduardo305@gmail.com>
2017-12-25 15:15:35 -0200

    Setting up react environment

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const webpack = require('webpack');
+console.log('path', path.resolve('/dist'));
+
+const config = {
+    entry: path.resolve(__dirname, 'src/index.js'),
+    devServer: {
+        hot: true,
+        inline: true,
+        port: 3000,
+        historyApiFallback: true,
+    },
+    output: {
+        path: '/dist',
+        filename: 'bundle.js',
+        publicPath: '/dist'
+    },
+    module: {
+        loaders: [{
+            test: /\.js?$/,
+            exclude: /node_modules/,
+            loader: "babel-loader",
+            query: {
+                presets: ['react', 'es2015', 'stage-0'] 
+            }
+        }]
+    }
+}
+
+module.exports = config;
